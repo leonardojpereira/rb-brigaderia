fc476e3e70c0e462d229fc614f64167d7dc50cf4
Leonardo Pereira <leonardobrpereira2015@gmail.com>
2024-11-08 20:53:58 -0300

    feat: criando pagina de entrada/saida e integrando rota get com paginacao e filtro de data

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -10,8 +10,13 @@ export class HeaderComponent {
   @Input() buttonText: string = '';
   @Output() buttonClick = new EventEmitter<void>();
   @Output() filterChange = new EventEmitter<string>();
+  @Output() dataInicialChange = new EventEmitter<string>();
+  @Output() dataFinalChange = new EventEmitter<string>();
   @Input() placeholder: string = '';
   @Input() isDisabled: boolean = false;
+  @Input() isButtonVisible: boolean = true;
+  @Input() isSearchFilterVisible: boolean = true;
+  @Input() isDateFilterVisible: boolean = false;
 
   onButtonClick(): void {
     this.buttonClick.emit();
@@ -20,5 +25,15 @@ export class HeaderComponent {
   onFilterInputChange(event: Event): void {
     const filterValue = (event.target as HTMLInputElement).value;
     this.filterChange.emit(filterValue);
+  }
+
+  onDataInicialChange(event: Event): void {
+    const dataInicial = (event.target as HTMLInputElement).value;
+    this.dataInicialChange.emit(dataInicial);
+  }
+
+  onDataFinalChange(event: Event): void {
+    const dataFinal = (event.target as HTMLInputElement).value;
+    this.dataFinalChange.emit(dataFinal);
   }
 }
